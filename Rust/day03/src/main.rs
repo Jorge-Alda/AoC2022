c14cd728506f274c63250db4e01df701022bcb96
Jorge-Alda <jalda@unizar.es>
2022-12-03 09:52:13 +0100

    Day 03 Part 1 Rust

---
+++ Rust/day03/src/main.rs
@@ -0,0 +1,64 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn priority(item: char) -> u32 {
+    let p = item as u32;
+    if p > 96 {
+        p-96
+    }else {
+        p-38
+    }
+}
+
+fn part1(input: String) -> u32 {
+    let mut tot: u32 = 0;
+    for rucksack in input.split('\n') {
+        let l = rucksack.len()/2;
+        let r1 = &rucksack[..l];
+        let r2 = &rucksack[l..];
+        for it1 in r1.chars() {
+            if r2.contains(it1) {
+                tot = tot + priority(it1);
+                break;
+            }
+        }
+    }
+    tot
+}
+
+fn main() {
+    
+    let path_st = Path::new("status");
+
+    let input = include_str!("../input");
+    let res = crate::part1(String::from(input.trim()));
+    
+    let path_o1 = Path::new("output1");
+    let mut file = File::create(path_o1).unwrap();
+    file.write_all(format!("{res}").as_bytes()).unwrap();
+
+    let mut file = File::create(path_st).unwrap();
+    file.write_all("1\n".as_bytes()).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_priority(){
+        let c1 = 'p';
+        let res1 = crate::priority(c1);
+        assert_eq!(res1, 16);
+        let c2 = 'L';
+        let res2 = crate::priority(c2);
+        assert_eq!(res2, 38);
+    }
+
+
+    #[test]
+    fn test_part1(){
+        let input = include_str!("../test");
+        let res = crate::part1(String::from(input.trim()));
+        assert_eq!(res, 157);
+    }
+}
