50b34c02503bfa191a9795cb4c8bd837f5c3f2b1
Jorge-Alda <jalda@unizar.es>
2022-12-04 01:05:33 +0100

    Day 03 Python: refactoring

--- Python/Day03/day03.py
+++ Python/Day03/day03.py
@@ -1,6 +1,24 @@
 from pathlib import Path
+from typing import TypeVar, Iterable
+from itertools import islice
 
 basepath = Path(__file__).parent
+
+T = TypeVar('T')
+
+
+def priority(c: str) -> int:
+    p = ord(c)
+    if p > 96:
+        return p-96
+    else:
+        return p-38
+
+
+def triplet(it: Iterable[T]) -> Iterable[tuple[T, T, T]]:
+    itt = iter(it)
+    while (batch := tuple(islice(itt, 3))):
+        yield batch
 
 
 def part1(inp: str) -> int:
@@ -11,12 +29,7 @@ def part1(inp: str) -> int:
         comp2 = rucksack[l:]
         for it in comp1:
             if it in comp2:
-                p = ord(it)
-                if p > 96:
-                    priority = (p-96)
-                else:
-                    priority = (p-38)
-                tot += priority
+                tot += priority(it)
                 break
     return tot
 
@@ -24,20 +37,11 @@ def part1(inp: str) -> int:
 def part2(inp: str) -> int:
     tot = 0
     rucksacks = inp.split('\n')
-    for i in range(int(len(rucksacks)/3)):
-        r1 = rucksacks[3*i]
-        r2 = rucksacks[3*i+1]
-        r3 = rucksacks[3*i+2]
+    for r1, r2, r3 in triplet(rucksacks):
         for it in r1:
-            if it in r2:
-                if it in r3:
-                    p = ord(it)
-                    if p > 96:
-                        priority = (p-96)
-                    else:
-                        priority = (p-38)
-                    tot += priority
-                    break
+            if (it in r2) and (it in r3):
+                tot += priority(it)
+                break
     return tot
 
 
