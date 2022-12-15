32956280d13faf743777d6926bce50674632dc4e
Jorge-Alda <jalda@unizar.es>
2022-12-15 12:40:01 +0100

    Day 15 Refactoring to interval class [WIP]

--- Python/Day15/day15.py
+++ Python/Day15/day15.py
@@ -1,7 +1,89 @@
 import re
+from itertools import product
+from copy import deepcopy
+from typing import Self
+from enum import Enum
 from pathlib import Path
 
 basepath = Path(__file__).parent
+
+
+class Extreme(Enum):
+    Start = 0
+    End = 1
+
+
+class Interval:
+    def __init__(self):
+        self.endpoints = []
+
+    @classmethod
+    def new(cls, start: int, end: int) -> Self:
+        res = cls()
+        if end > start:
+            res.endpoints = [(start, Extreme.Start), (end, Extreme.End)]
+        else:
+            res.endpoints = [(end, Extreme.Start), (start, Extreme.End)]
+        return res
+
+    def __len__(self) -> int:
+        l = 0
+        for i in range(len(self.endpoints)//2):
+            l += self.endpoints[2*i+1] - self.endpoints[2*i] + 1
+        return l
+
+    def __repr__(self) -> str:
+        return ", ".join(f"[{str(self.endpoints[2*i])}, {self.endpoints[2*i+1]}]" for i in range(len(self.endpoints)//2))
+
+    def __or__(self, rhs: Self) -> Self:
+        if not isinstance(rhs, Interval):
+            raise ValueError
+        res = Interval()
+        if len(self.endpoints) == 0:
+            res.endpoints = rhs.endpoints
+        else:
+            combined = sorted(self.endpoints + rhs.endpoints,
+                              key=lambda p: p[0])
+            s = 0
+            start_val = 0
+            new_endpoints = []
+            for p in combined:
+                if s == 0 and p[1] == Extreme.Start:
+                    start_val = p[0]
+                    s += 1
+                elif s == 1 and p[1] == Extreme.End:
+                    new_endpoints.append((start_val, Extreme.Start))
+                    new_endpoints.append((p[0], Extreme.End))
+                    s = 0
+                elif p[1] == Extreme.Start:
+                    s += 1
+                else:
+                    s -= 1
+            res.endpoints = new_endpoints
+        return res
+
+
+"""     def __or__(self, rhs: Self) -> Self:
+        if not isinstance(rhs, Interval):
+            raise ValueError
+        res = Interval()
+        if len(self.starts) == 0:
+            res.starts = rhs.starts
+            res.ends = rhs.ends
+        elif rhs.starts[0] > self.ends[-1]:
+            res.starts = self.starts + rhs.starts
+            res.ends = self.ends + rhs.ends
+        elif rhs.ends[-1] < self.starts[0]:
+            res.starts = rhs.starts + self.starts
+            res.ends = rhs.ends + self.ends
+        else:
+            for i in range(len(self.starts)):
+                if self.starts[i] < rhs.starts[0] and self.ends[i] > rhs.ends[-1]:
+                    res.starts = self.starts
+                    res.ends = self.ends
+
+        #old_endpoints = deepcopy(self.endpoints)
+        return res """
 
 
 def parse_line(l: str) -> tuple[tuple[int, int], tuple[int, int]]:
@@ -26,16 +108,39 @@ def covered_sensor(sensor: tuple[int, int],
     return (sensor[0]-(d-d_target), sensor[0]+(d-d_target))
 
 
-def part1(input: str, target: int) -> int:
+def scan_line(sensors: list[tuple[int, int]],
+              beacons: list[tuple[int, int]],
+              target: int) -> set[int]:
     covered = set()
-    for l in input.split('\n'):
-        sensor, beacon = parse_line(l)
+    for sensor, beacon in zip(sensors, beacons):
         try:
             range_y = covered_sensor(sensor, beacon, target)
             covered |= set(range(range_y[0], range_y[1]))
         except ValueError:
             pass
-    return len(covered)
+    return covered
+
+
+def part1(input: str, target: int) -> int:
+    sensors, beacons = [], []
+    for l in input.split('\n'):
+        sensor, beacon = parse_line(l)
+        sensors.append(sensor)
+        beacons.append(beacon)
+    return len(scan_line(sensors, beacons, target))
+
+
+def part2(input: str, dim: int) -> int:
+    sensors, beacons, dists = [], [], []
+    for l in input.split('\n'):
+        sensor, beacon = parse_line(l)
+        sensors.append(sensor)
+        beacons.append(beacon)
+        dists.append(dist(sensor, beacon))
+    for x, y in product(range(dim+1), range(dim+1)):
+        if not any(dist((x, y), s) <= d for s, d in zip(sensors, dists)):
+            return 4_000_000*x+y
+    return 0
 
 
 if __name__ == '__main__':
--- Python/Day15/test_15.py
+++ Python/Day15/test_15.py
@@ -1,4 +1,4 @@
-from day15 import part1
+from day15 import part1, part2, Interval
 from pathlib import Path
 
 basepath = Path(__file__).parent
@@ -9,3 +9,17 @@ def test_part1():
         inp = f.read().strip()
     out = part1(inp, 10)
     assert out == 26
+
+
+def test_part2():
+    with open(basepath/"test", "rt") as f:
+        inp = f.read().strip()
+    out = part2(inp, 20)
+    assert out == 56000011
+
+
+def test_interval():
+    i1 = Interval.new(9, 11)
+    i2 = Interval.new(10, 13)
+    itot = Interval.new(9, 13)
+    assert (i1 | i2) == itot
